df1fd8fb64587115bae999c7f41474770821f48e
guripong <guripong@hanmail.net>
2024-03-14 17:08:22 +0900

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,31 @@
-import { GPprops, GPtable } from './lib'
+// import { GPprops, GPtable ,isOdd} from './lib';
 // import { GPtable } from "./dist/index.es.js"; // 빌드된 파일의 경로로 변경해야 합니다.
 // import type { GPprops } from "./dist";
-
+import { GPprops, GPtable, isOdd } from 'react-gptable'
 function App() {
     // GPprops 타입의 속성을 생성합니다.
-    const props: GPprops = {
-        a: "Hello",
-        b: 123,
-        c: { a: 456, b: "World" },
-    };
+    // const props: GPprops = {
+    //     a: "Hello",
+    //     b: 123,
+    //     c: { a: 456, b: "World" },
+    // };
 
-    return (
-        <>
-            사용예시
-            <GPtable {...props} /> {/* GPtable 컴포넌트에 props 전달 */}
-        </>
-    )
+    // return (
+    //     <>
+    //         사용예시
+    //         <GPtable {...props} /> {/* GPtable 컴포넌트에 props 전달 */}
+    //     </>
+    // )
+    return (<>
+    {"isOdd사용후:"+isOdd(4)}
+    <GPtable 
+    
+        a="Hello"
+        b={1234444}
+        c={{ a: 456, b: "World"}}
+    
+    />
+    </>)
 }
 
 export default App;
